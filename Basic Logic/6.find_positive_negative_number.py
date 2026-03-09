2aeaab1591c7f29ce5ea60c7ad1ad5684004099a
Akash82945 <akashkumar9576386319@gmail.com>
2026-03-09 23:16:33 +0530

    This is the Day 6 practice set.

---
+++ Basic Logic/6.find_positive_negative_number.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Positive or negative Number: "))
+
+if num == 0:
+    print(f"Number is equal to Zero.")
+    
+elif num < 0:
+    print("Number is Negative.")
+    
+else:
+    print("Number is Positive.")
