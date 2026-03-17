24b9b9a0c52efeb551893f037e78b8cfb6558ac2
Akash82945 <akashkumar9576386319@gmail.com>
2026-03-17 22:38:34 +0530

    Function practioce.

---
+++ Function/51.find_mamium_of_three_number.py
@@ -0,0 +1,16 @@
+def maximum_number(number1, number2, number3):
+    
+    if number1 > number2 or number1 > number3:
+        return number1
+    elif number2 > number3 or number2 > number1:
+        return number2
+    else:
+        return number3
+    
+number1 = int(input("Enter number first: "))
+number2 = int(input("Enter second first: "))
+number3 = int(input("Enter third first: "))
+
+result = maximum_number(number1,number2,number3)
+
+print(result)
